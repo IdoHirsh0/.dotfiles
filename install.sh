f1b50a556445b6571267893a266dff03c1c1aec2
Ido Hirsh <itsidohirsh@gmail.com>
2022-09-16 14:54:49 +0300

    orgenized install.sh

--- install.sh
+++ install.sh
@@ -1,62 +1,83 @@
 #!/bin/bash
 
-# Installation & Configuration of my environment on a fresh machine!
 
 # Updating & Upgrading
+
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
-# Install stow
+
+# Installations
+
+## Install stow
 sudo apt-get install stow -y
 
-# Install neovim
+## Install neovim
 sudo add-apt-repository ppa:neovim-ppa/unstable -y
 sudo apt-get update -y
 sudo apt-get install neovim -y
 
-# Install the C package
+## Install the C package
 sudo apt install build-essential -y
 sudo apt-get install manpages-dev -y
 
-# Install C language server
+## Install C language server
 sudo apt install ccls -y
 
-# Install neofetch
+## Install neofetch
 sudo apt install neofetch -y
 
-# Make .config and .local/share directories in ~ ,if doesn't exist already, to make stow symlink only the wanted directories
+
+# Configurations
+
+## Create .config and .local/share directories in ~, if doesn't exist already, to make stow symlink only the wanted directories
 mkdir ~/.config
 mkdir ~/.local/share -p
 
+## Make an empty .git directory in root to make the LSP autocomplition work
+sudo mkdir /.git
+
+## Make neofetch launch on logon
+echo '' >> ~/.bashrc
+echo '# Make neofetch launch on logon' >> ~/.bashrc
+echo 'neofetch --ascii_distro Windows7' >> ~/.bashrc
+
+
 # Better bash prompt
+
+## Install powerline fonts
 sudo apt install fonts-powerline -y
-cd ~
-git clone --recursive https://github.com/andresgongora/synth-shell.git
-chmod +x synth-shell/setup.sh
-cd synth-shell
-./setup.sh
+
+## Install synth-shell
+git clone --recursive https://github.com/andresgongora/synth-shell.git ~/synth-shell
+
+## Run synth shell setup wizard
+chmod +x ~/synth-shell/setup.sh
+~/synth-shell/setup.sh
+
+## Delete ~/synth-shell
+rm -rf ~/synth-shell
 
 ## Remove synth-shell-prompt.config after installation so I can use my own synth-shell-prompt.config with stow
 rm ~/.config/synth-shell/synth-shell-prompt.config
 
-# Stow
-cd ~/.dotfiles
-stow bash
-stow git
-stow synth-shell
-stow nvim
-stow fonts
-sudo stow -d ~/.dotfiles/ -t / clang-format/
 
-# Install neovim plugins
+# Stow
+
+sudo stow --dir=~/.dotfiles/ --target=~/ ~/.dotfiles/bash
+sudo stow --dir=~/.dotfiles/ --target=~/ ~/.dotfiles/git
+sudo stow --dir=~/.dotfiles/ --target=~/ ~/.dotfiles/synth-shell
+sudo stow --dir=~/.dotfiles/ --target=~/ ~/.dotfiles/nvim
+sudo stow --dir=~/.dotfiles/ --target=~/ ~/.dotfiles/fonts
+
+## Stow clang-format to / to make it global
+sudo stow --dir=~/.dotfiles/ --target=/  ~/.dotfiles/clang-format
+
+
+# Post Stow
+
+## Install neovim plugins
 nvim --headless +PlugInstall +qall
 
-# Make an empty .git directory in root to make the lsp autocomplition work
-sudo mkdir /.git
-
-# Make neofetch launch on logon
-echo '' >> ~/.bashrc
-echo '# Make neofetch launch on logon' >> ~/.bashrc
-echo 'neofetch --ascii_distro Windows7' >> ~/.bashrc
