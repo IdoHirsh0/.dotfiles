7fa606d50992f06778782593e24e7318d0c91206
Ido Hirsh <itsidohirsh@gmail.com>
2022-09-18 01:43:08 +0300

    install packer from inside of vim

---
+++ install/nvim.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Install
+sudo add-apt-repository -y ppa:neovim-ppa/unstable
+sudo apt-get update -y
+sudo apt-get install  -y neovim
--- nvim/.config/nvim/lua/itsidohirsh/plugins.lua
+++ nvim/.config/nvim/lua/itsidohirsh/plugins.lua
@@ -1,10 +1,26 @@
-return require('packer').startup(function()
-    --packer can manage itself
+-- Automatically install packer
+local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+    PACKER_BOOTSTRAP = vim.fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim',
+        install_path })
+    print("Installing packer close and reopen Neovim...")
+end
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+    return
+end
+
+return packer.startup(function(use)
+    -- Packer can manage itself
     use 'wbthomason/packer.nvim'
 
     -- LSP
     use 'neovim/nvim-lspconfig'
-    --- Autocomplition
+
+    -- Autocomplition
     use 'hrsh7th/nvim-cmp'
     use 'hrsh7th/cmp-nvim-lsp'
     use 'L3MON4D3/LuaSnip'
@@ -22,7 +38,12 @@ return require('packer').startup(function()
     use 'Xuyuanp/nerdtree-git-plugin'
     use 'ryanoasis/vim-devicons'
 
-    -- colorschemes
+    -- Colorschemes
     use 'folke/tokyonight.nvim'
     use 'sainnhe/gruvbox-material'
+
+    -- Automatically set up configuration after cloning packer.nvim
+    if PACKER_BOOTSTRAP then
+        require('packer').sync()
+    end
 end)
