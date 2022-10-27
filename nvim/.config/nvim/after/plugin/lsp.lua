45bb9a3837da0a3e390216c59ce656262716c51f
Ido Hirsh <itsidohirsh@gmail.com>
2022-10-27 16:59:21 +0300

    updated deprecated command of vim lsp

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -73,7 +73,7 @@ vim.lsp.protocol.make_client_capabilities().textDocument.completion.completionIt
 
 local function config(_config)
     return vim.tbl_extend('force', {
-        capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+        capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities()),
         on_attach = on_attach
     }, _config or {})
 end
